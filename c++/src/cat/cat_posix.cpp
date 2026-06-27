1a16491555bd4e48eb26aace2840b874ed9836f7
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-06-27 18:25:32 +0530

    [UNIX-LABS] feat: Implemented cat using pure posix apis in C++

---
+++ c++/src/cat/cat_posix.cpp
@@ -0,0 +1,42 @@
+#include <array>
+#include <cerrno>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "cat_posix.hpp"
+
+constexpr size_t BUFFER_SIZE = 8192;
+
+void copy_fd(int fd) {
+  std::array<std::byte, BUFFER_SIZE> buffer;
+
+  while (true) {
+    ssize_t bytes_read = read(fd, buffer.data(), buffer.size());
+
+    if (bytes_read == 0) {
+      return;
+    }
+
+    if (bytes_read < 0) {
+      perror("read");
+      return;
+    }
+
+    ssize_t bytes_written = 0;
+
+    while (bytes_written < bytes_read) {
+      ssize_t n = write(STDOUT_FILENO, buffer.data() + bytes_written,
+                        bytes_read - bytes_written);
+
+      if (n < 0) {
+        perror("write");
+        return;
+      }
+
+      bytes_written += n;
+    }
+  }
+}
---
+++ c++/src/cat/main.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "cat_posix.hpp"
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    copy_fd(STDIN_FILENO);
+    return 0;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    int fd = open(argv[i], O_RDONLY);
+
+    if (fd < 0) {
+      perror(argv[i]);
+      continue;
+    }
+
+    copy_fd(fd);
+    close(fd);
+  }
+
+  return 0;
+}
