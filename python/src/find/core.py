1676a827f3e3d169a5409c22bb91db00d0c8051a
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-09-18 21:57:51 +0530

    [UNIX-LABS] fix: Implemented find command core logic

--- python/src/find/core.py
+++ python/src/find/core.py
@@ -1,4 +1,33 @@
+from pathlib import Path
+from dataclasses import dataclass
+from enum import Enum
+
+
+class EntityType(Enum):
+    FILE = "f"
+    DIRECTORY = "d"
+
+
+@dataclass
+class EntityInfo:
+    name: str
+    type: EntityType
+    path: str
+
+
+def is_valid_entity(entity_info: EntityInfo, filters) -> bool:
+    return all(filter(entity_info) for filter in filters)
+
 
 def find(search_path, filters):
+    search_path_dir = Path(search_path)
 
+    for entity in search_path_dir.rglob("*"):
+        entity_info = EntityInfo(
+            name=entity.name,
+            type=EntityType.FILE if entity.is_file() else EntityType.DIRECTORY,
+            path=str(entity),
+        )
 
+        if is_valid_entity(entity_info, filters=filters):
+            print(f"./{entity_info.path}" if search_path == "." else entity_info.path)
